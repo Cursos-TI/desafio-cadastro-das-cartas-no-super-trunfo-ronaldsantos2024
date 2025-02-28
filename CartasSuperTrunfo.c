d7bedd233ec5016e92970fe9ef12b660eeea21a9
ronaldsantos2024 <ronald__as@hotmail.com>
2025-02-28 18:45:22 +0000

    iniciando o cadastro das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,1 +1,17 @@
- #
+ #include <stdio.h>
+
+ int main (){
+    char Estado;
+    char [40] Código da carta;
+    char [40] Nome da cidade;
+    int População;
+    float Área;
+    float PIB;
+    int Número de Pontos_Turisticos;
+
+    printf("Cadastro das cartas do Super Trunfo!\n");
+    printf("Carta 1:\n");
+
+
+
+ }
